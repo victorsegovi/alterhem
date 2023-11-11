eef173356abede934be8e3d0f7da5f31958dfd3b
victorsegovia <victoralejandrost881@gmail.com>
2023-11-11 15:49:58 -0400

    Added interactivity in the Services Section

--- js/services.js
+++ js/services.js
@@ -1,13 +1,11 @@
-const services = document.querySelectorAll('.service')
+const services = document.querySelectorAll('.service');
 
-services.forEach((service)=>{
+services.forEach(service=>{
     service.addEventListener('click', ()=>{
         if(service.className == 'service'){
-            service.removeAttribute('class', 'service')
             service.setAttribute('class', 'dropdown')
-        }else if(service.className == 'dropdown'){
-            service.removeAttribute('class', 'dropdown')
-            service.setAttribute('class', 'service')
+        }else{
+            service.setAttribute('class','service')
         }
     })
 })
